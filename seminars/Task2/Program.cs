d3ed79318da5b60512dcb416cdd9869c2262c365
Kosyachenko Olga <kosya4enko.o@yandex.ru>
2022-08-28 21:46:01 +0300

    task3

---
+++ seminars/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.Clear();
+
+Console.Write (" Введите число от 0 до 99999: ");
+
+int n =  int.Parse(Console.ReadLine());
+
+if(n>0 && n<100)
+{
+   Console.WriteLine("Третьей цифры нет");
+}
+
+else if (n>=100 && n<1000)
+{
+   int n2= n %10;
+   Console.WriteLine(n2);
+}
+else  if (n>=1000 && n<10000)
+{ int n2   =   n/ 10 ;
+
+int f  = n2%10;
+
+   Console.WriteLine(f);
+
+}
+else if (n>=10000 && n<100000)
+     { int n7   =   n/ 100 ;
+
+
+int f0 = n7%10;
+
+   Console.WriteLine(f0);
+   }
+
+ else
+ {Console.WriteLine(" Веедите другое число, отвечающее условиям ");
+    
+}
---
+++ seminars/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите цифру, обозначающую день недели");
+
+int n =int.Parse(Console.ReadLine());
+
+if (n==6 || n==7){
+    Console.WriteLine("Да, выходной");
+
+}
+else if (n>0 && n<=5) {
+
+    Console.WriteLine("Нет, рабочий день");
+
+}
+else {
+    Console.WriteLine("В неделе 7 дней");
+}
