167f92b9e8aa99dc542e76a89da54b908599da51
Kosyachenko Olga <kosya4enko.o@yandex.ru>
2022-08-28 22:22:17 +0300

    task2

--- seminars/Task2/Program.cs
+++ seminars/Task2/Program.cs
@@ -36,3 +36,25 @@ int f0 = n7%10;
  {Console.WriteLine(" Веедите другое число, отвечающее условиям ");
     
 }
+/*
+Console.Clear();
+
+Console.WriteLine("Введите число ");
+
+int n = int.Parse(Console.ReadLine());
+
+if (n < 100){
+    Console.WriteLine($"Третьей цифры нет");
+
+    return ;
+}
+int numberLength = n.ToString().Length;
+
+int divider =1;
+ for (int i =3 ; i < numberLength; i++){
+    divider *=10;
+ }
+ int num = (n / divider);
+ Console.WriteLine($"Третья цифра  числа {n} -> { num %10}");
+
+ */
